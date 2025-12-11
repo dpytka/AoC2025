101536c7a7da43abf16dea3904bee70381dba6ef
Dawid Pytka <dpytka@gmail.com>
2025-12-11 18:28:57 +0100

    Day 11 p 2

---
+++ day11/reactor_2.py
@@ -0,0 +1,33 @@
+def all_simple_paths(graph, start, goal):
+    result = []
+    visited = set()
+    path = []
+
+    def dfs(u):
+        if u in visited:
+            return
+
+        visited.add(u)
+        path.append(u)
+
+        if u == goal:
+            if 'fft' in path and 'dac' in path:
+                print("goal found")
+                result.append(list(path))
+        else:
+            for v in graph.get(u, []):
+                dfs(v)
+
+        path.pop()
+        visited.remove(u)
+
+    dfs(start)
+    return result
+
+lines = [line.strip().split(':') for line in open("input.txt","r").readlines()]
+graph = dict()
+for line in lines:
+    graph[line[0]] = line[1].strip().split(" ")
+
+res = all_simple_paths(graph, "svr", "out")
+print(len(res))
